d74257f804b467533052694ecf587e4f9b1e6431
hhiyul <rkdrnr9945@gmail.com>
2024-11-14 10:33:45 +0900

    움직임 수정
    ㅇㅇ

--- Assets/Simple 2D Platformer BE2/Sprites/char_move.cs
+++ Assets/Simple 2D Platformer BE2/Sprites/char_move.cs
@@ -7,8 +7,8 @@ public class char_move : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D rigid2D;
     float jumpForce = 270.0f;
-    float walkForce = 50.0f;
-    float maxWalkSpeed = 4.0f;
+    float walkForce = 6.0f;
+    float maxWalkSpeed = 3.0f;
 
     int maxJumpCount = 2;
     int jumpCount = 0;
@@ -18,10 +18,9 @@ public class char_move : MonoBehaviour
 
     }
 
-    // Update is called once per frame  
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount) //점프
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
             jumpCount++;
@@ -29,17 +28,32 @@ public class char_move : MonoBehaviour
 
 
         int key = 0;
-        if(Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if(Input.GetKey(KeyCode.LeftArrow)) key = -1;
-        
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)) key = 1;
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)) key = -1;
 
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
-        if(speedx < this.maxWalkSpeed || IsInAir())
+        if (!IsInAir())
         {
-            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+            if (speedx < this.maxWalkSpeed)
+            {
+                this.rigid2D.velocity = new Vector2(key * maxWalkSpeed, this.rigid2D.velocity.y);
+            }
         }
-
+        else
+        {
+        // 공중에서 속도가 maxWalkSpeed를 초과하지 않도록 제한
+            if (speedx < maxWalkSpeed)
+            {
+            // 공중에서도 일정한 속도 내에서 이동 가능하게 설정
+                this.rigid2D.AddForce(transform.right * key * this.walkForce);
+            }
+            else
+            {
+            // 속도 제한을 넘는 경우에는 maxWalkSpeed로 고정
+                this.rigid2D.velocity = new Vector2(key * maxWalkSpeed, this.rigid2D.velocity.y);
+            }
+        }   
         if (key != 0)
         {
             transform.localScale = new Vector3(key, 1, 1); // 방향에 따라 캐릭터를 좌우 반전
